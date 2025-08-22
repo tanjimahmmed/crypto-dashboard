6bd9e99608bb2d62eb8072f4da6d101eecc31cb3
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-08-22 15:46:23 +0600

    remove old method add try and catch method to show the data

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-    .then((res) => {
-      if(!res.ok) throw new Error('Failed to fetch data');
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      setCoins(data)
-      setLoading(false)
-    })
-    .catch((err) => {
-      setError(err.message)
-      setLoading(false)
-    })
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if(!res.ok) throw new Error('Failed to fetch data')
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch(err){
+        setError(err.message)
+      }finally {
+        setLoading(false)
+      }
+    }
+    fetchCoins();
   }, []);
 
   return (
